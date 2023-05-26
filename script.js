05607f65e7b4bc05038d78164c2f5ae501d3f716
Reyner Veliz <reynerv@yahoo.com>
2023-05-26 09:07:17 -0600

    Form validation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const modal = document.getElementById('modal');
 const modalShow = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
-const bookmarkForm = document.getElementById('bookmard-form');
+const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
@@ -17,3 +17,35 @@ const showModal = () => {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+const validate = (nameValue, urlValue) => {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if(!nameValue || !urlValue) {
+        alert('Please submit values for both fields.');
+        return false;
+    }
+    if(!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    return true;
+}
+
+// Handle Data from Form
+const storeBookmark = e => {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+     urlValue = `https://${urlValue}`; 
+}
+    console.log(nameValue, urlValue);
+    if(!validate(nameValue, urlValue)) {
+        return false;
+    }
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
